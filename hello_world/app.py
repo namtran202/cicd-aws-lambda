955afa3bd0d3c3f304e6d86cc76d6092d05a1e0e
Nam Tran <thnam200298@gmail.com>
2022-10-04 15:53:30 +0700

    edit

--- hello_world/app.py
+++ hello_world/app.py
@@ -14,6 +14,7 @@ def lambda_handler(event, context):
         service_name="secretsmanager",
         region_name=region
     )
+    
     get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     secrets_response = get_secret_value_response["SecretString"]
     string = json.loads(secrets_response)["key1"]
