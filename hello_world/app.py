58a22cff2799c610f57e70ca4e978af5443a970d
Nam Tran <thnam200298@gmail.com>
2022-10-04 11:22:43 +0700

    commit

--- hello_world/app.py
+++ hello_world/app.py
@@ -10,7 +10,11 @@ def lambda_handler(event, context):
     secret_name = "my-secrets-manager-08"
 
     session = boto3.session.Session()
-    client = session.client(service_name="secretsmanager", region_name=region)
+    client = session.client(
+        service_name="secretsmanager", 
+        region_name=region
+    )
+    
     get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     secrets_response = get_secret_value_response["SecretString"]
     string = json.loads(secrets_response)["key1"]
